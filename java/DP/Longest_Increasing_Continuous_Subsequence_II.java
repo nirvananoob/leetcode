15495923b4bcf034d53760460524f11581494de5
kai zhang <emmonenirvana@gmail.com>
2016-10-18 00:00:04 -0500

    done for find all paths

--- java/DP/Longest_Increasing_Continuous_Subsequence_II.java
+++ java/DP/Longest_Increasing_Continuous_Subsequence_II.java
@@ -78,47 +78,72 @@ public class Longest_Increasing_Continuous_Subsequence_II {
 	// }
 
 	// v2 dp
-	public int longestIncreasingContinuousSubsequenceII(int[][] A) {
-		// Write your code here
-		if (A == null || A.length == 0 || A[0].length == 0) {
-			return 0;
-		}
-		int[][] res = new int[A.length][A[0].length];
-		boolean[][] visited = new boolean[A.length][A[0].length];
-		int[] dx = { 1, -1, 0, 0 };
-		int[] dy = { 0, 0, 1, -1 };
-		int max = 1;
-		for (int i = 0; i < res.length; i++) {
-			for (int j = 0; j < res[0].length; j++) {
-				res[i][j] = search(i, j, A, res, visited, dx, dy);
-				max = Math.max(res[i][j], max);
-			}
-		}
-		return max;
-
-	}
-
-	private int search(int row, int col, int[][] arr, int[][] res,
-			boolean[][] visited, int[] dx, int[] dy) {
-		if (visited[row][col]) {
-			return res[row][col];
-		}
-		res[row][col] = 1;
-		visited[row][col] = true;
-		// int nx, ny;
-		for (int i = 0; i < dx.length; i++) {
-			if (row + dx[i] >= 0 && row + dx[i] < arr.length
-					&& col + dy[i] >= 0 && col + dy[i] < arr[0].length
-					&& arr[row + dx[i]][col + dy[i]] > arr[row][col]) {
-				res[row][col] = Math.max(
-						res[row][col],
-						search(row + dx[i], col + dy[i], arr, res, visited, dx,
-								dy) + 1);
-			}
-		}
-		return res[row][col];
-	}
-
+    private int[] dx = {1, 0, -1, 0};
+    private int[] dy = {0, 1, 0, -1};
+    public int longestIncreasingPath(int[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0 ) {
+            return 0;
+        }
+        int[][] lens = new int[matrix.length][matrix[0].length];
+        
+        int max = 0;
+        for(int i = 0; i < lens.length; i++){
+            for(int j = 0 ; j < lens[0].length; j++){
+                max = Math.max(max, search(matrix, lens, i, j));
+            }
+        }
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<Integer> ();
+        for(int i = 0; i < lens.length; i++){
+            for(int j = 0 ; j < lens[0].length; j++){
+                if(lens[i][j] == max){
+                    list.add(matrix[i][j]);
+                    printpath(lens, matrix, i, j,res,list,max);
+                    list.remove(list.size() -1);
+                }
+            }
+        }
+        for(int i = 0 ; i < lens.length; i++){
+            System.out.println(Arrays.toString(lens[i]));
+        }
+        System.out.println(res.toString());
+        return max;
+    }
+    private int search(int[][] matrix, int[][] res, int x, int y) {
+        
+        if(res[x][y] != 0){
+            
+            return res[x][y];
+        }
+        int nx, ny;
+        res[x][y] = 1;
+        for(int i = 0 ; i < dx.length; i++){
+            nx = x + dx[i];
+            ny = y + dy[i];
+            if(nx >= 0 && nx < res.length && ny >= 0  && ny < res[0].length && matrix[nx][ny] < matrix[x][y]) {
+                res[x][y] = Math.max(search(matrix, res, nx, ny) + 1, res[x][y] );
+            }
+        }
+        return res[x][y];
+    }
+    private  void printpath(int[][] arr, int[][] mat, int x, int y, ArrayList<List<Integer>> ans,List<Integer> list,int max){
+        if(arr[x][y] == 1 && list.size() == max){
+            ans.add(new ArrayList<Integer> (list));
+            return;
+        }
+        int nx, ny;
+        for(int i = 0 ; i < dx.length; i++){
+            nx = x + dx[i];
+            ny = y + dy[i];
+            if(nx >= 0 && nx < mat.length && ny >= 0  && ny < mat[0].length && arr[nx][ny] ==  arr[x][y] - 1 && mat[x][y] > mat[nx][ny]) {
+                list.add(0,mat[nx][ny]);
+                printpath(arr,mat, nx, ny,ans,list,max);
+                list.remove(0);
+            }
+        }
+        return;
+        
+    }
 	public static void main(String[] args) {
 		int[][] arr = { { 1, 2, 3, 4, 5 }, { 16, 17, 24, 23, 6 },
 				{ 15, 18, 25, 22, 7 }, { 14, 19, 20, 21, 8 },
